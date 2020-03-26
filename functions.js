fa34931c33cecf3b938ca1fc7eebac54a3f8582f
xuexishiwokuaile <37232631+xuexishiwokuaile@users.noreply.github.com>
2020-03-26 13:38:01 +0800

    Add files via upload

--- functions.js
+++ functions.js
@@ -271,3 +271,17 @@ function MaxNum(str)
     }
     return maxLetter;
 }
+
+function sumBigNumber(a, b) 
+{
+    var res = '',
+      temp = 0;
+    a = a.split('');
+    b = b.split('');
+    while (a.length || b.length || temp) {
+      temp += ~~a.pop() + ~~b.pop();
+      res = (temp % 10) + res;
+      temp = temp > 9;
+    }
+    return res.replace(/^0+/, '');
+}
